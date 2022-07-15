b6ec7c90fd1100351532e767ecd75b9336ada9ff
tuva9 <tuva9@fpt.edu.vn>
2022-07-15 07:38:00 +0700

    Product table

--- cellphone/src/pages/Admin/Product/product.tsx
+++ cellphone/src/pages/Admin/Product/product.tsx
@@ -1,12 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
-import { Typography, Button } from 'antd';
-import {Link} from 'react-router-dom'
+import { Typography, Button, Table } from 'antd';
+import { Link } from 'react-router-dom'
 import { SearchOutlined, PlusOutlined } from '@ant-design/icons';
-const {Paragraph}  = Typography
+const { Paragraph } = Typography
+import type { ColumnsType } from 'antd/es/table';
+import { getAll } from "../../../api/product";
+
+interface DataType {
+    name: string;
+    saleOffPrice: number;
+    feature: string;
+    description: string;
+}
+
+const columns: ColumnsType<DataType> = [
+    {
+        title: 'Tên sản phẩm',
+        dataIndex: 'name',
+        key: 'name',
+        render: text => <a>{text}</a>,
+    },
+    {
+        title: 'Đặc điểm',
+        dataIndex: 'feature',
+        key: 'feature',
+        render: text => <a>{text}</a>,
+    },
+    {
+        title: 'Giá khuyến mãi',
+        dataIndex: 'saleOffPrice',
+        key: 'saleOffPrice',
+    },
+    {
+        title: 'Mô tả',
+        dataIndex: 'description',
+        key: 'description',
+    },
+];
+
 
 
 const ProductAdminPage = () => {
+    const [dataTable, setDataTable] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getAll()
+                setDataTable(data.data)
+            } catch (err) {
+
+            }
+        }
+        
+        fetchData()
+    }, [])
     return (
         <>
             <Breadcrumb>
@@ -17,6 +65,7 @@ const ProductAdminPage = () => {
                     <Button type="dashed" shape="circle" icon={<PlusOutlined />} />
                 </Link>
             </Breadcrumb>
+            <Table columns={columns} dataSource={dataTable} />
         </>
     )
 }
