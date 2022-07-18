bd65cf739c037d647f0274a3cb714d0242a36fc0
tuva9 <tuva9@fpt.edu.vn>
2022-07-18 08:18:33 +0700

    demo: Upload image

--- cellphone/src/pages/Admin/Product/edit.tsx
+++ cellphone/src/pages/Admin/Product/edit.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import { message } from 'antd';
+import React, { useState } from 'react';
+import { upload } from '../../../api/images';
 
 const EditProduct: React.FC = () => {
+  const [previewImage, setPreviewImage] = useState('')
+  const handleChangeImage = (e: any) => {
+    console.log(e.target.files)
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+      uploadImage(reader.result)
+    }
+  }
+
+  const uploadImage = async (base64Image: string) => {
+    try {
+      const data = await upload(base64Image)
+      console.log(data)    
+    } catch(err) {
+      console.log(err)
+      message.error("upload image fail")
+    }
+    
+  }
   return (
     <>
+      <input
+        type="file"
+        accept='image/png, image/jpg, image/jpeg' onChange={handleChangeImage} />
+      <img src={previewImage} alt="" />
     </>
   );
 };
