82b368d86c29042704368c8c40dc1d9b6a55eda5
tuva9 <tuva9@fpt.edu.vn>
2022-07-21 10:55:16 +0700

    Feature: SWR mutate

--- cellphone/src/hooks/useProduct.ts
+++ cellphone/src/hooks/useProduct.ts
@@ -1,5 +1,16 @@
+import { message } from "antd"
+import useSWR from "swr"
+import { getAll } from "../api/product"
 const useProduct = () => {
-
-}
+    const {data, error, mutate} = useSWR('/products', getAll)
+    if(error) {
+        message.error(error.message)
+    }
+    return {
+        data,
+        isLoading: !error && !data,
+        isError: error
+    }
+}   
 
 export default useProduct
