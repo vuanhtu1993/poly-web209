2cbc6a39f6d85174b22e604efc1df44d5b9f101b
tuva9 <tuva9@fpt.edu.vn>
2023-06-26 08:28:25 +0700

    update

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -1,35 +1,35 @@
 const app = document.querySelector('#app')
 
-const component = function () {
-    const [status, setStatus] = React.useState(false)
-    return (
-        <div>
-            <img style={{ maxWidth: 80 }} src={status ? "./images/on.png" : "./images/off.png"} />
-            <button onClick={() => setStatus(!status)}>Switch</button>
-        </div>
-    )
-}
-
-const root = ReactDOM.createRoot(app)
-root.render(React.createElement(component))
-
-
-// const img = document.createElement("img")
-// img.src = "./images/off.png"
-// img.style.maxWidth = "80px"
-// const bulkSwitch = document.createElement("button")
-// bulkSwitch.innerText = "Turn on"
-
-// app.appendChild(img)
-// app.appendChild(bulkSwitch)
-
-// bulkSwitch.onclick = function () {
-//     if (img.src == "http://127.0.0.1:5500/lesson1/images/off.png") {
-//         img.src = "./images/on.png"
-//     } else {
-//         img.src = "./images/off.png"
-//     }
+// const component = function () {
+//     const [status, setStatus] = React.useState(false)
+//     return (
+//         <div>
+//             <img style={{ maxWidth: 80 }} src={status ? "./images/on.png" : "./images/off.png"} />
+//             <button onClick={() => setStatus(!status)}>Switch</button>
+//         </div>
+//     )
 // }
+
+// const root = ReactDOM.createRoot(app)
+// root.render(React.createElement(component))
+
+
+const img = document.createElement("img")
+img.src = "./images/off.png"
+img.style.maxWidth = "80px"
+const bulkSwitch = document.createElement("button")
+bulkSwitch.innerText = "Turn on"
+
+app.appendChild(img)
+app.appendChild(bulkSwitch)
+
+bulkSwitch.onclick = function () {
+    if (img.src == "http://127.0.0.1:5500/lesson1/images/off.png") {
+        img.src = "./images/on.png"
+    } else {
+        img.src = "./images/off.png"
+    }
+}
 
 // let _status = false
 
