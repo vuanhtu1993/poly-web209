584d74f1114bb6496438a21557298d6ee555fc51
tuva9 <tuva9@fpt.edu.vn>
2023-06-27 17:25:37 +0700

    Buổi 1: Imperative and declarative

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -1,14 +1,13 @@
 const app = document.querySelector('#app')
 
-// Imperative
-// const bulb = document.createElement("img")
+// // Imperative
+// const bulb = document.createElement('img')
 // bulb.src = "./images/off.png"
 // bulb.style.maxWidth = "80px"
-// const switchBtn = document.createElement("button")
+// const switchBtn = document.createElement('button')
 // switchBtn.innerText = "Switch"
 // // Event
-// switchBtn.onclick = () => {
-
+// switchBtn.onclick = function () {
 //     if (bulb.src == "http://127.0.0.1:5500/lesson1/images/off.png") {
 //         bulb.src = "./images/on.png"
 //     } else {
@@ -19,16 +18,15 @@ const app = document.querySelector('#app')
 // app.appendChild(bulb)
 // app.appendChild(switchBtn)
 
-
 // Declarative
 let state = true
 
-const render = () => {
+const render = function () {
     app.innerHTML = `
         <img style="max-width: 80px" src="${state ? "./images/on.png" : "./images/off.png"}"/>
         <button>Switch</button>
     `
-    document.querySelector("button").onclick = function () {
+    document.querySelector('button').onclick = function () {
         state = !state
         render()
     }
