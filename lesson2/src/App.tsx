63e6658fb04bb3aafded1052868aa0ed781b8d00
tuva9 <tuva9@fpt.edu.vn>
2023-06-28 15:50:25 +0700

    Buổi 2: Render data

--- lesson2/src/App.tsx
+++ lesson2/src/App.tsx
@@ -1,4 +1,5 @@
 import FilmCard from "./components/filmCard"
+import data from '../db'
 
 function App() {
   return <div>
@@ -6,14 +7,12 @@ function App() {
       <img className="w-[80px]" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="" />
     </header>
     <div className="content pt-4 grid grid-cols-3 gap-4">
-      <FilmCard title="" year={ } thumbnail="" />
-      <FilmCard />
-      <FilmCard />
-      <FilmCard />
-      <FilmCard />
-      <FilmCard />
-      <FilmCard />
-      <FilmCard />
+      {data.map(film => <FilmCard
+        key={film.title}
+        title={film.title}
+        year={film.year}
+        thumbnail={film.thumbnail}
+      />)}
     </div>
   </div>
 }
--- lesson2/src/components/filmCard.tsx
+++ lesson2/src/components/filmCard.tsx
@@ -1,22 +1,22 @@
 
 type Film = { title: string, year: number, thumbnail: string }
 
-const FilmCard = function (props: Film) {
+const FilmCard = function ({ title, year, thumbnail }: Film) {
     return <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
         <img
             alt="Office"
-            src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            src={thumbnail}
             className="h-56 w-full object-cover"
         />
 
         <div className="bg-white p-4 sm:p-6">
             <time className="block text-xs text-gray-500">
-                10th Oct 2022
+                {year}
             </time>
 
             <a href="#">
                 <h3 className="mt-0.5 text-lg text-gray-900">
-                    How to position your furniture for positivity
+                    {title}
                 </h3>
             </a>
 
