6a1df739c1d2d4b0177bb5a5b55f79e19b2680ed
tuva9 <tuva9@fpt.edu.vn>
2023-06-28 09:00:06 +0700

    Buổi 2: Vite react typescript

--- lesson2/src/App.tsx
+++ lesson2/src/App.tsx
@@ -1,10 +1,20 @@
-function App() {
+import Film from "./components/film"
 
-  return (
-    <>
-      <h1 className="bg-slate-400">Welcome to react app</h1>
-    </>
-  )
+function App() {
+  return <div>
+    <header>
+      <img className="w-[80px]" src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Netflix_logo.png" alt="" />
+    </header>
+    <div className="pt-4 grid grid-cols-3 gap-2">
+      {/* Render dynamic - props*/}
+      <Film title="" year={ } thumbnail="" />
+      <Film />
+      <Film />
+      <Film />
+      <Film />
+      <Film />
+    </div>
+  </div>
 }
 
 export default App
