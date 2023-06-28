87762ea82b1976e130555195e24f54f81433746f
tuva9 <tuva9@fpt.edu.vn>
2023-06-28 15:22:03 +0700

    Buổi 2: Props

--- lesson2/src/App.tsx
+++ lesson2/src/App.tsx
@@ -1,10 +1,21 @@
-function App() {
+import FilmCard from "./components/filmCard"
 
-  return (
-    <>
-      <h1 className="bg-slate-400">Welcome to react app</h1>
-    </>
-  )
+function App() {
+  return <div>
+    <header className="flex">
+      <img className="w-[80px]" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="" />
+    </header>
+    <div className="content pt-4 grid grid-cols-3 gap-4">
+      <FilmCard title="" year={ } thumbnail="" />
+      <FilmCard />
+      <FilmCard />
+      <FilmCard />
+      <FilmCard />
+      <FilmCard />
+      <FilmCard />
+      <FilmCard />
+    </div>
+  </div>
 }
 
 export default App
