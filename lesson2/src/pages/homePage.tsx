1d0d48733dce54bb9213fbbf90fe08883b8b815f
tuva9 <tuva9@fpt.edu.vn>
2023-06-29 10:13:56 +0700

    Buổi 3: Setup axios

--- lesson2/src/pages/homePage.tsx
+++ lesson2/src/pages/homePage.tsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react"
 import FilmCard from "../components/filmCard"
+import { IFilm } from "../models"
+import { getAll } from "../api/films"
 
 const HomePage = function () {
+    const [films, setFilms] = useState<IFilm[]>([])
+
+    const getFilms = async function () {
+        const data = await getAll()
+        setFilms(data)
+    }
+
+    useEffect(() => {
+        getFilms()
+    }, [])
+
     return <div className="grid grid-cols-3 gap-4">
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
-        <FilmCard />
+        {films.map(item => <FilmCard key={item.id} film={item} />)}
     </div>
 }
 
