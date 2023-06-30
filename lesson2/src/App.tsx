fca3ef145f65299c82127d6dc4f9b62f346848f8
tuva9 <tuva9@fpt.edu.vn>
2023-06-30 15:11:06 +0700

    Buổi 3: useLoaderData

--- lesson2/src/App.tsx
+++ lesson2/src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/home'
 import FilmPage from './pages/film'
+import { getAll } from './api/film'
 
 const routers = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />
+    element: <HomePage />,
+    loader: async () => {
+      const films = await getAll()
+      return { films }
+    }
   },
   { path: '/product/:id', element: <FilmPage /> },
 ])
--- lesson2/src/pages/home.tsx
+++ lesson2/src/pages/home.tsx
@@ -1,20 +1,9 @@
-import { useEffect, useState } from 'react'
 import FilmCard from '../components/filmCard'
-import { getAll } from '../api/film'
 import { IFilm } from '../models'
+import { useLoaderData } from 'react-router-dom'
 
 const HomePage = function () {
-    const [films, setFilms] = useState<IFilm[]>([])
-    // useEffect
-    useEffect(() => {
-        const getFilm = async () => {
-            const data = await getAll()
-            setFilms(data)
-            console.log(data);
-
-        }
-        getFilm()
-    }, [])
+    const { films } = useLoaderData() as { films: IFilm[] }
     return <div>
         <header className="flex">
             <img className="w-[80px]" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="" />
