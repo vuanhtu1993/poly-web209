aebf0ffa586b2a81d61fa9fa7e8aab0e3c9657b7
tuva9 <tuva9@fpt.edu.vn>
2023-06-30 08:27:12 +0700

    Buổi 3: Apply axios

--- lesson2/src/App.tsx
+++ lesson2/src/App.tsx
@@ -1,18 +1,28 @@
+import { useEffect, useState } from "react"
 import Film from "./components/film"
+import { getAll } from "./api/film"
 
 function App() {
+  const [films, setFilms] = useState([])
+  // useEffect:
+  // 1. Tham số thứ 2 = undefined
+  // 2. Tham số thứ 2 = []
+  // 3. Tham số thứ 2 phụ thuộc vào props hoặc state
+  useEffect(() => {
+    const getFilms = async () => {
+      const data = await getAll()
+      setFilms(data)
+    }
+    getFilms()
+  }, [])
   return <div>
     <header>
-      <img className="w-[80px]" src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Netflix_logo.png" alt="" />
+      <img className="w-[80px]" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="" />
     </header>
     <div className="pt-4 grid grid-cols-3 gap-2">
       {/* Render dynamic - props*/}
-      <Film title="" year={ } thumbnail="" />
-      <Film />
-      <Film />
-      <Film />
-      <Film />
-      <Film />
+      {films.map((item) => <Film data={item} />)}
+
     </div>
   </div>
 }
--- lesson2/src/components/film.tsx
+++ lesson2/src/components/film.tsx
@@ -1,22 +1,29 @@
 
-type IFilm = { title: string, year: number, thumbnail: string }
+type Props = {
+    data: {
+        title: string,
+        year: number,
+        thumbnail: string
+    },
+    children?: React.ReactNode
+}
 
-const Film = function (props: IFilm) {
+const Film = function ({ data }: Props) {
     return <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
         <img
             alt="Office"
-            src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            src={data.thumbnail}
             className="h-56 w-full object-cover"
         />
 
         <div className="bg-white p-4 sm:p-6">
             <time className="block text-xs text-gray-500">
-                10th Oct 2022
+                {data.year}
             </time>
 
             <a href="#">
                 <h3 className="mt-0.5 text-lg text-gray-900">
-                    How to position your furniture for positivity
+                    {data.title}
                 </h3>
             </a>
 
