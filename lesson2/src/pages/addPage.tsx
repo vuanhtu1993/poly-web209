a0433c3a5b97377cb0af8502872b12724b1ee78a
tuva9 <tuva9@fpt.edu.vn>
2023-07-08 23:07:07 +0700

    Buổi 4: Validate form as original

--- lesson2/src/pages/addPage.tsx
+++ lesson2/src/pages/addPage.tsx
@@ -1,91 +1,72 @@
+import { ReactElement, useState } from "react"
+
 const AddFilmPage = function () {
+  const [title, setTitle] = useState("")
+  const [isValidTitle, setValidTitle] = useState(false)
+
+  const handleTitle = (e: React.FormEvent) => {
+    const value = (e.target as HTMLInputElement).value
+    validateTitle(value)
+    setTitle(value)
+  }
+
+  const validateTitle = (title: string) => {
+    if (title != "") {
+      setValidTitle(true)
+    } else {
+      setValidTitle(false)
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (!isValidTitle) {
+      alert("Vui lòng kiểm tra lại!!!")
+    }
+  }
+
   return <section className="relative flex flex-wrap lg:h-screen lg:items-center">
     <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
       <div className="mx-auto max-w-lg text-center">
-        <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
-
-        <p className="mt-4 text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
-          eaque error neque ipsa culpa autem, at itaque nostrum!
-        </p>
+        <h1 className="text-2xl font-bold sm:text-3xl">Thêm mới</h1>
       </div>
 
-      <form action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+      <form action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4" onSubmit={handleSubmit}>
         <div>
-          <label className="sr-only">Email</label>
+          <label className="sr-only">Title</label>
 
           <div className="relative">
             <input
-              type="email"
-              className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-              placeholder="Enter email"
+              type="text"
+              className="w-full border rounded-lg border-black p-4 pe-12 text-sm shadow-sm"
+              placeholder="Title"
+              onChange={handleTitle}
+              name="title"
             />
-
-            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                />
-              </svg>
-            </span>
           </div>
+          <div className="text-red-400">{!isValidTitle && "Trường dữ liệu không hợp lệ"}</div>
         </div>
 
         <div>
-          <label className="sr-only">Password</label>
+          <label className="sr-only">Extract</label>
 
           <div className="relative">
-            <input
-              type="password"
-              className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-              placeholder="Enter password"
-            />
-
-            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                />
-              </svg>
-            </span>
+            <textarea
+              className="w-full border rounded-lg border-black p-4 pe-12 text-sm shadow-sm"
+              placeholder="Extract"
+              cols={6}
+              name="extract"
+            ></textarea>
           </div>
         </div>
 
         <div className="flex items-center justify-between">
-          <p className="text-sm text-gray-500">
-            No account?
-            <a className="underline" href="">Sign up</a>
-          </p>
 
           <button
             type="submit"
             className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
           >
-            Sign in
+            Thêm mới
           </button>
         </div>
       </form>
@@ -94,7 +75,7 @@ const AddFilmPage = function () {
     <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
       <img
         alt="Welcome"
-        src="https://images.unsplash.com/photo-1630450202872-e0829c9d6172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        src="https://images.thedirect.com/media/article_full/mcu-watch-order-marvel-studios-first-time_pR7gM4t.jpg"
         className="absolute inset-0 h-full w-full object-cover"
       />
     </div>
